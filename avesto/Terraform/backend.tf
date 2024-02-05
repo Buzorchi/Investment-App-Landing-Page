45edfcfb010f8e7627c2101bbc1a4c2d118d0595
Emmanuel Olukayode Omonijo <eomonijo877@gmail.com>
2024-02-05 23:15:02 +0000

    Updated backend.tf

--- avesto/Terraform/backend.tf
+++ avesto/Terraform/backend.tf
@@ -1,8 +1,10 @@
 terraform {
-    backend "azurerm" = {
-      resource_group_name = "Terra_State_Store"
-      storage_account_name = "terrastatestore2024"
-      container_name = "terracontainerstore"
-      key = "terraform.tfstate"
-    }
-}  
+  backend "azurerm" {
+    resource_group_name   = "Terra_State_Store"
+    storage_account_name   = "terrastatestore2024"
+    container_name         = "terracontainerstore"
+    key                    = "terraform.tfstate"
+  }
+}
+
+

43adf12b729b3e2b845e531fef46b3dddcd04a15
Emmanuel Olukayode Omonijo <eomonijo877@gmail.com>
2024-02-06 10:37:36 +0000

    Updated main.tf

--- avesto/Terraform/main.tf
+++ avesto/Terraform/main.tf
@@ -9,8 +9,8 @@ terraform {
 
 provider "azurerm" {
   subscription_id = "1bbe1d16-db50-421b-9afb-771f42577508"
-  client_id       = "875f80ef-6c88-458d-9135-b630e031fb57"
-  client_secret   = "1fb8Q~vqzIEY7HRZOVKQjVeZFmi3mAS4Uw1Uxams"
+  client_id       = "885fdcf9-0a05-42a8-8d10-79a7156e3930"
+  client_secret   = "aqw8Q~yF-GX7EeS9Zo75mNSufSwezGW8IQsp-agY"
   tenant_id       = "7b2f922c-33b1-4c77-a870-99bfb9429a4d"
   features {}
 }
@@ -29,6 +29,8 @@ resource "azurerm_app_service_plan" "App_Plan" {
   name                = "app-plan10"
   location            = local.location
   resource_group_name = local.resource_group_name
+  kind = "Linux"
+  reserved = true
   depends_on = [ azurerm_resource_group.Resource_Group ]
 
   sku {
